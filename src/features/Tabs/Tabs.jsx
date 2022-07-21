1138b8e3922b600ef85cf520d03b9c7b2c1f35cd
Wen Chen <wen.chen@ailabpte.co>
2022-07-21 16:00:54 +0800

    refactor codes

---
+++ src/features/Tabs/Tabs.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import './Tabs.css'
+import logo from '../../svgs/logo.svg'
+import { MediaLengthCalculator } from './components/MediaLengthCaculator'
+import { LabelCalculator } from './components/LabelCalculator'
+
+function Tabs() {
+	//toggle state
+	const [toggleState, setToggleState] = useState(1)
+	const toggleTab = index => {
+		setToggleState(index)
+	}
+
+	return (
+		<div className='container'>
+			<div>
+				<img className='img' src={logo} alt='' />
+			</div>
+			<div className='bloc-tabs'>
+				<button
+					className={toggleState === 1 ? 'tabs active-tabs' : 'tabs'}
+					onClick={() => toggleTab(1)}
+					id='tab1'
+				>
+					Media Length Calculator
+				</button>
+				<button
+					className={toggleState === 2 ? 'tabs active-tabs' : 'tabs'}
+					onClick={() => toggleTab(2)}
+					id='tab2'
+				>
+					Label Calculator
+				</button>
+			</div>
+
+			<div className='content-tabs'>
+				<MediaLengthCalculator toggleState={toggleState} />
+				<LabelCalculator toggleState={toggleState} />
+			</div>
+		</div>
+	)
+}
+
+export default Tabs
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Tabs from './Tabs';
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Tabs from './features/Tabs/Tabs'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Tabs />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<React.StrictMode>
+		<Tabs />
+	</React.StrictMode>,
+	document.getElementById('root')
+)
