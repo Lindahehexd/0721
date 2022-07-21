ad133740c89ab1e0139acae60812a475c5843bd8
lindalin <103926856+Lindahehexd@users.noreply.github.com>
2022-07-21 11:30:30 +0800

    0721

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Tabs from './Tabs';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Tabs />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
